0b6dc45c8e4be73ffb1dbf9cbab5a50ef15fedb9
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-09-22 14:06:39 +0100

    mod path

--- ensign_utils/utils.py
+++ ensign_utils/utils.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import request, redirect, render_template
 from fastai.learner import load_learner
-from fastai.vision.core import parent_label
+# from fastai.vision.core import parent_label
+import fastai.vision.core
 
 
 def parent_label_list(label):
-    return [parent_label(label)]
+    return [fastai.vision.core.parent_label(label)]
