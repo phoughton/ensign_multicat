570f2ebc73c9ffe15591b7105233c38df6c5b2f2
Peter Houghton <pete@investigatingsoftware.co.uk>
2020-09-22 14:08:25 +0100

    limit imports

--- ensign_utils/utils.py
+++ ensign_utils/utils.py
@@ -1,7 +1,3 @@
-from flask import Flask
-from flask import request, redirect, render_template
-from fastai.learner import load_learner
-# from fastai.vision.core import parent_label
 import fastai.vision.core
 
 
